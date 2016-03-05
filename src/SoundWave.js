3fbd6e1b1447ede07f3ac533afb26daccc9124f1
RolandJansen <jansen@stereokrauts.com>
2016-03-05 01:02:14 +0100

    SoundWave: XHR loading and appending AudioBuffers; basic docgen.

--- src/SoundWave.js
+++ src/SoundWave.js
@@ -1,32 +1,175 @@
 'use strict';
 
-var SoundWave = function(audioCtx, binaryData, collection) {
+var core = require('./core.js');
 
-  this.audioCtx = null;
-  this.buffer = null;
+/**
+ * Creates a wrapper in which an audio buffer lives.
+ * It can handle one or more ArrayBuffers or filenames
+ * (*.wav, *.mp3) as data sources.
+ * Multiple sources will be concatenated into one audio buffer.<br>
+ * You normally shouldn't use any of the provided methods.
+ * A SoundWave object just holds audio data and does nothing.
+ * If you want to play the sound, you have to additionally create a Sound object.
+ * @example <caption>Play a sound from an audio file:</caption>
+ * var soundWave = new Intermix.SoundWave('file.wav');
+ * var sound = new Intermix.Sound(soundWave.buffer);
+ * sound.play;
+ * @example <caption>concatenate multiple source files into one buffer<br>
+ * in the given order and play them:</caption>
+ * var soundWave = new Intermix.SoundWave('file1.wav,file2.wav,file3.wav');
+ * var sound = new Intermix.Sound(soundWave.buffer);
+ * sound.play;
+ * @example <caption>
+ * Using ArrayBuffers instead of filenames will come in handy if you want<br>
+ * to have full control over XHR or use a preloader (here: preload.js):
+ * </caption>
+ * var queue = new createjs.LoadQueue();
+ * queue.on('complete', handleComplete);
+ * queue.loadManifest([{id: 'src1', src:'file1.wav', type:createjs.AbstractLoader.BINARY},
+ *     {id:"src2", src:"file2.wav", type:createjs.AbstractLoader.BINARY}]);
+ *
+ * function handleComplete() {
+ *     var binData1 = queue.getResult('src1');
+ *     var binData2 = queue.getResult('src2');
+ *     var wave1 = new Intermix.SoundWave(binData1);
+ *     var wave2 = new Intermix.SoundWave(binData2);
+ *     var concatWave = new Intermix.SoundWave([binData1, binData2]);
+ * };
+ * @constructor
+ * @param  {(Object|Object[]|string)} audioSrc   One or more ArrayBuffers or filenames
+ */
+var SoundWave = function(audioSrc) {
 
+  this.audioCtx = core.audioCtx;  //window.AudioContext
+  this.buffer = null;             //AudioBuffer
 
-  if (audioCtx) {
-    this.audioCtx = audioCtx;
+  if (audioSrc) {
+    if (audioSrc instanceof ArrayBuffer) {
+      //one audio buffer to decode
+      this.decodeAudioData(audioSrc);
+    } else if (audioSrc instanceof Array && audioSrc[0] instanceof ArrayBuffer) {
+      //multiple audio buffers to decode and concatenate
+      this.concatBinariesToAudioBuffer(audioSrc);
+    } else if (typeof audioSrc === 'string' && audioSrc.indexOf(',') === 0) {
+      //one file to load/decode
+      var binBuffer;
+      this.loadFile(audioSrc, function(response) {
+        binBuffer = response;
+      });
+      this.buffer = this.decodeAudioData(binBuffer);
+    } else if (typeof audioSrc === 'string' && audioSrc.indexOf(',') > -1) {
+      //multiple files to load/decode and cancatinate
+      var binBuffers = this.loadFiles(audioSrc);
+      this.concatBinariesToAudioBuffer(binBuffers);
+    }
   } else {
-    console.log('No AudioContext found');
+    throw new Error('Cannot create SoundWave object: Binary data missing.');
   }
 
+};
+
+/**
+ * Takes binary audio data and turns it into an audio buffer object.
+ * This is a wrapper for the web-audio-api decodeAudioData function.
+ * @param  {ArrayBuffer} rawAudioSrc Audio data in raw binary format
+ * @return {AudioBuffer}             Ready to use audio buffer
+ */
+SoundWave.prototype.decodeAudioData = function(rawAudioSrc) {
+  return this.audioCtx.decodeAudioData(rawAudioSrc).then(function(decoded) {
+    return decoded;
+  });
+};
+
+/**
+ * Concatenates one or more ArrayBuffers to an AudioBuffer.
+ * @param  {Array} binaryBuffers  Array holding one or more ArrayBuffers
+ * @param  {AudioBuffer} audioBuffer   An existing AudioBuffer object
+ * @return {AudioBuffer}               The concatenated AudioBuffer
+ */
+SoundWave.prototype.concatBinariesToAudioBuffer = function(binaryBuffers, audioBuffer) {
   var self = this;
 
-  if (binaryData) {
-    this.audioCtx.decodeAudioData(binaryData).then(function(decoded) {
-      self.buffer = decoded;
-      console.log('buffer created');
+  binaryBuffers.forEach(function(binBuffer) {
+    var tmpAudioBuffer = self.decodeAudioData(binBuffer);
+    audioBuffer = self.appendAudioBuffer(self.Buffer, tmpAudioBuffer);
+  });
+
+  return audioBuffer;
+};
+
+/**
+ * Appends two audio buffers. Suggested by Chris Wilson:
+ * http://stackoverflow.com/questions/14143652/web-audio-api-append-concatenate-different-audiobuffers-and-play-them-as-one-son
+ * @param  {AudioBuffer} buffer1 The first audio buffer
+ * @param  {AudioBuffer} buffer2 The second audio buffer
+ * @return {AudioBuffer}         buffer1 + buffer2
+ */
+SoundWave.prototype.appendAudioBuffer = function(buffer1, buffer2) {
+  var numberOfChannels = Math.min(buffer1.numberOfChannels, buffer2.numberOfChannels);
+  var tmp = this.audioCtx.createBuffer(numberOfChannels,
+    (buffer1.length + buffer2.length),
+    buffer1.sampleRate);
+  for (var i = 0; i < numberOfChannels; i++) {
+    var channel = tmp.getChannelData(i);
+    channel.set( buffer1.getChannelData(i), 0);
+    channel.set( buffer2.getChannelData(i), buffer1.length);
+  }
+  return tmp;
+};
+
+/**
+ * Loads a binary file and calls a function with the
+ * returned ArrayBuffer as its argument when done.
+ * @param  {string}   filename       The file to be loaded
+ * @param  {function} onloadCallback The function to be called
+ * @param  {boolean}  [sync=true]   Asynchronous loading
+ * @example
+ * var arrayBuffer;
+ * this.loadFile('file1.wav', function(response) {
+ *   arrayBuffer = response;
+ * });
+ */
+SoundWave.prototype.loadFile = function(filename, onloadCallback, sync) {
+  var asynchronously = true;
+  var request = new window.XMLHttpRequest();
+
+  if (sync) {
+    asynchronously = sync;
+  }
+
+  request.open('GET', filename, asynchronously);
+  request.responseType = 'arraybuffer';
+
+  request.onload = function() {
+    onloadCallback(request.response);
+  };
+
+  request.send();
+};
+
+/**
+ * Loads multiple binary files and returns an array
+ * with the data from the files in the given order.
+ * @param  {Array}  filenames List with filenames
+ * @return {Array}            Array of ArrayBuffers
+ */
+SoundWave.prototype.loadFiles = function(filenames) {
+  var self = this;
+  var binBuffers = [];
+  var names = filenames.split(',');
+  names.forEach(function(name) {
+    self.loadFile(name, function(response) {
+      binBuffers[name] = response;
     });
-  } else {
-    //TODO: throw error
-  }
+  });
 
-  if (collection) {
-    //TODO: put buffer into soundbank
-  }
+  return this.sortBinBuffers(names, binBuffers);
+};
 
+SoundWave.prototype.sortBinBuffers = function(filenames, binBuffers) {
+  return filenames.map(function(el) {
+    binBuffers[el];
+  });
 };
 
 module.exports = SoundWave;
