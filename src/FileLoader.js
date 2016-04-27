b688ff2f66d20d7927f4a0bd0b8f80a8b909dd62
RolandJansen <jansen@stereokrauts.com>
2016-04-27 12:56:12 +0200

    Added code coverage and reorganized tasks

---
+++ src/FileLoader.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Build a loader with promises and xhr. See:
+ * http://stackoverflow.com/questions/30008114/how-do-i-promisify-native-xhr
+ * https://blog.pivotal.io/labs/labs/testing-javascript-promises
+ */
+var FileLoader = function() {
+
+};
+
+module.exports = FileLoader;
