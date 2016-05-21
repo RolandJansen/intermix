6ccb63b6612439435aa5afef3bb5468c56002ff3
RolandJansen <jansen@stereokrauts.com>
2016-05-21 04:37:30 +0200

    EventBus essentials implemented w/ tests.

--- src/EventBus.js
+++ src/EventBus.js
@@ -1,7 +1,46 @@
 'use strict';
 
+/**
+ * This is an event bus that can be used to connect
+ * intermix (and other) components.
+ * It has the usual message events that you can
+ * subscribe/unsubscribe to like in Backbone, jquery etc.
+ * The more interesting part is a relay
+ * system that can be used to share control data between
+ * relay attendees.
+ * An attendee has to add itself to a relay to become
+ * a relay endpoint. Now, all other bus attendees can
+ * read out its controllers and send/receive data to/from it.
+ * The event bus has two buildin relays 'instrument' and 'fx',
+ * others can be added.
+ * Sounds complex? Here are some examples:
+ * @example <caption>Registering to a relay<caption>
+ * var myNewInstrument = function(eventBus) {
+ *
+ *   this.ctrlSpec = {
+ *     'title': String,
+ *     'volume': [0, 127],
+ *     'pan': [-63, 64],
+ *   };
+ *
+ *   this.uid = eventBus.addRelayEndpoint('instrument', ctrlSpec, this);
+ *   ...
+ * @example <caption>Sending data to the relay is easy</caption>
+ *   ...
+ *   eventBus.fireEvent({'volume': 23}, uid);
+ *   ...
+ * @example <caption>To receive events you have to implement 'handleRelayData'</caption>
+ *   ...
+ *   this.handleRelayData = function(data) {
+ *     // do something with the data
+ *   };
+ * };
+ * @constructor
+ * @return {Void}
+ */
 var EventBus = function() {
 
+  this.lookup = {};
   this.relays = {
     'instrument': {},
     'fx': {}
@@ -25,41 +64,103 @@ EventBus.prototype.addRelayEndpoint = function(relay, data, context) {
     throw new Error('Missing argument "context"');
   } else {
     this.relays[relay][uid] = {
-      'uid': uid,
       'context': context,
-      'data': data
+      'data': data,
+      'relay': relay,
+      'subscribers': []
     };
-    this.fireEvent('onRelayAdd', relay, uid);
+    this.lookup[uid] = this.relays[relay][uid];
+    this.sendMessage('onRelayAdd', relay, uid);
 
     return uid;
   }
 
 };
 
-EventBus.prototype.removeRelayEndpoint = function(relay, context) {
-
-};
-
-EventBus.prototype.subscribe = function() {
-
-};
-
-EventBus.prototype.fireEvent = function(msg, relay, uid) {
-
-};
-
-EventBus.prototype.notifyAttendees = function(relay, msg, uid) {
-  if (typeof uid === 'string') {
-    this.relays[relay]
+EventBus.prototype.removeRelayEndpoint = function(relay, uid) {
+  if (this.relays[relay].hasOwnProperty(uid)) {
+    delete this.relays[relay][uid];
+    delete this.lookup[uid];
+  } else {
+    throw new Error('uid not found in relay ' + relay);
   }
 };
 
-EventBus.prototype.getRoot = function() {
+EventBus.prototype.getEndpointSpec = function(uid) {
+  return this.lookup[uid].data;
+};
 
+EventBus.prototype.getAllRelayEndpointSpecs = function(relay) {
+  if (typeof this.relays[relay] !== 'undefined') {
+    var specs = [];
+    for (var uid in this.relays[relay]) {
+      specs.push(this.relays[relay][uid].data);
+    }
+    return specs;
+  } else {
+    throw new TypeError('Relay: ' + relay + ' not found');
+  }
+};
+
+EventBus.prototype.sendToRelay = function(relay, msg) {
+  for (var uid in this.relays[relay]) {
+    this.sendToRelayEndpoint(uid, msg);
+  }
+};
+
+EventBus.prototype.sendToRelayEndpoint = function(uid, msg) {
+  if (typeof this.lookup[uid] !== 'undefined') {
+    var endpoint = this.lookup[uid].context;
+    endpoint.handleRelayData.call(endpoint, msg);
+  }
+  //fails silently if uid is wrong or undefined
+};
+
+EventBus.prototype.getRelayNames = function() {
+  var names = [];
+  for (var name in this.relays) {
+    names.push(name);
+  }
+  return names;
+};
+
+EventBus.prototype.subscribe = function(msg, fn, context) {
+  if (typeof msg === 'string' &&
+  typeof fn === 'function' &&
+  typeof context !== 'undefined') {
+    if (typeof this.messages[msg] === 'undefined') {
+      this.messages[msg] = [];
+    }
+    this.messages[msg].push({ 'context': context, 'fn': fn });
+    return true;
+  } else {
+    throw new TypeError('One or more arguments of wrong type or missing');
+  }
+};
+
+EventBus.prototype.unsubscribe = function(msg, context) {
+  if (typeof context !== 'undefined') {
+    var message = this.messages[msg];
+    message.forEach(function(subscriber, index) {
+      if (subscriber.context === context) {
+        message.splice(index, 1);
+      }
+    });
+  } else {
+    throw new TypeError('context is undefined');
+  }
+};
+
+EventBus.prototype.sendMessage = function(msg, data) {
+  var subscribers = this.messages[msg];
+  var length = subscribers.length;
+  for (var i = 0; i < length; i++) {
+    subscribers[i].fn.call(subscribers[i].context, data);
+  }
 };
 
 /**
- * Creates an 16 byte random number represented as a string
+ * Creates 16 bytes random data represented as a string
  * @return {String} Random, binary-like UID
  */
 EventBus.prototype.getUID = function() {
@@ -76,7 +177,15 @@ EventBus.prototype.getUID = function() {
   return uid;
 };
 
-// as suggested by RobG: http://stackoverflow.com/questions/5876332/how-can-i-differentiate-between-an-object-literal-other-javascript-objects
+
+/**
+ * Tests if an object is a plain javascript object (object literal)
+ * and not a constructor, instance, null or anything else.
+ * as suggested by RobG:
+ * http://stackoverflow.com/questions/5876332/how-can-i-differentiate-between-an-object-literal-other-javascript-objects
+ * @param  {Object} obj Any javascript object
+ * @return {Boolean}    True if plain js object, false if not
+ */
 EventBus.prototype.isPlainObject = function(obj) {
   if (typeof obj === 'object' && obj !== null) {
     var proto = Object.getPrototypeOf(obj);
--- test/spec/EventBusSpec.js
+++ test/spec/EventBusSpec.js
@@ -3,30 +3,46 @@
 var EventBus = require('../../src/EventBus.js');
 
 describe('EventBus', function() {
+  var dataDef = {
+    'note': [0, 127],
+    'volume': [0, 127],
+    'pan': [-63, 64],
+    'title': String
+  };
 
-  describe('subscribers object', function() {
+  describe('relays object', function() {
 
     beforeEach(function() {
       this.eb = new EventBus();
     });
 
-    it('has a property "instrument" that is a plain object', function() {
+    it('has a property "instrument" of type plain object', function() {
       expect(this.eb.isPlainObject(this.eb.relays.instrument)).toBeTruthy();
     });
 
-    it('has a property "fx" that is a plain object', function() {
+    it('has a property "fx" of type plain object', function() {
       expect(this.eb.isPlainObject(this.eb.relays.fx)).toBeTruthy();
     });
 
   });
 
+  describe('messages object', function() {
+
+    beforeEach(function() {
+      this.eb = new EventBus();
+    });
+
+    it('has a property "onRelayAdd" of type Array', function() {
+      expect(this.eb.messages.onRelayAdd).toEqual(jasmine.any(Array));
+    });
+
+    it('has a property "onRelayRemove" of type Array', function() {
+      expect(this.eb.messages.onRelayRemove).toEqual(jasmine.any(Array));
+    });
+
+  });
+
   describe('.addRelayEndpoint', function() {
-    var dataDef = {
-      'note': [0, 127],
-      'volume': [0, 127],
-      'pan': [-63, 64],
-      'title': String
-    };
 
     beforeEach(function() {
       this.eb = new EventBus();
@@ -36,7 +52,7 @@ describe('EventBus', function() {
 
       beforeEach(function() {
         // this.eb.subscribe('onRelayAdd', this.onRelayAddHandler, this);
-        this.eb.fireEvent = jasmine.createSpy('fireEvent');
+        this.eb.sendMessage = jasmine.createSpy('fireEvent');
         this.uid = this.eb.addRelayEndpoint('instrument', dataDef, this);
       });
 
@@ -50,20 +66,23 @@ describe('EventBus', function() {
         expect(def.data).toEqual(dataDef);
       });
 
-      it('extends the def object with its uid', function() {
-        var def = this.eb.relays.instrument[this.uid];
-        expect(def.hasOwnProperty('uid')).toBeTruthy();
-        expect(def.uid.length).toEqual(32);
-      });
-
       it('extends the def object with the context', function() {
         var def = this.eb.relays.instrument[this.uid];
         expect(def.hasOwnProperty('context')).toBeTruthy();
         expect(def.context).toEqual(this);
       });
 
+      it('extends the def object with the name to its relay', function() {
+        var def = this.eb.relays.instrument[this.uid];
+        expect(def.relay).toEqual('instrument');
+      });
+
+      it('adds a reference to the relay endpoint to a lookup table', function() {
+        expect(this.eb.lookup[this.uid]).toEqual(this.eb.relays.instrument[this.uid]);
+      });
+
       it('fires an "onRelayAdd" event', function() {
-        expect(this.eb.fireEvent).toHaveBeenCalledWith('onRelayAdd', 'instrument', this.uid);
+        expect(this.eb.sendMessage).toHaveBeenCalledWith('onRelayAdd', 'instrument', this.uid);
       });
 
     });
@@ -98,6 +117,312 @@ describe('EventBus', function() {
   describe('.removeRelayEndpoint', function() {
 
     beforeEach(function() {
+      this.eb = new EventBus();
+      this.uid = this.eb.addRelayEndpoint('instrument', dataDef, this);
+    });
+
+    describe('on success', function() {
+
+      beforeEach(function() {
+        this.eb.removeRelayEndpoint('instrument', this.uid);
+      });
+
+      it('removes the endpoint from relay', function() {
+        expect(Object.keys(this.eb.relays.instrument).length).toEqual(0);
+      });
+
+      it('removes the endpoint from the lookup table', function() {
+        expect(Object.keys(this.eb.lookup).length).toEqual(0);
+      });
+
+    });
+
+    describe('on failure', function() {
+
+      it('if uid is missing or wrong, throws error', function() {
+        var self = this;
+        expect(function() {
+          self.eb.removeRelayEndpoint('instrument');
+        }).toThrowError('uid not found in relay instrument');
+      });
+
+      it('if relay is missing or wrong, throws a TypeError', function() {
+        var self = this;
+        expect(function() {
+          self.eb.removeRelayEndpoint();
+        }).toThrowError(TypeError);
+      });
+
+    });
+
+  });
+
+  describe('.getEndpointSpec', function() {
+
+    beforeEach(function() {
+      this.eb = new EventBus();
+      this.uid = this.eb.addRelayEndpoint('instrument', dataDef, this);
+    });
+
+    it('on success returns an endpoint specification', function() {
+      var spec = this.eb.getEndpointSpec(this.uid);
+      expect(spec).toEqual(dataDef);
+    });
+
+    it('on failure throws a TypeError', function() {
+      var self = this;
+      expect(function() {
+        self.eb.getEndpointSpec();
+      }).toThrowError(TypeError);
+    });
+
+  });
+
+  describe('.getAllRelayEndpointSpecs', function() {
+    var def2 = { 'volume': [0, 127] };
+
+    beforeEach(function() {
+      this.eb = new EventBus();
+      this.uid1 = this.eb.addRelayEndpoint('instrument', dataDef, this);
+      this.uid2 = this.eb.addRelayEndpoint('instrument', def2, this);
+    });
+
+    it('on success returns an array with all endpoint specs', function() {
+      var specs = this.eb.getAllRelayEndpointSpecs('instrument');
+      expect(specs.length).toEqual(2);
+      expect(specs).toContain(dataDef);
+      expect(specs).toContain(def2);
+    });
+
+    it('on failure returns a TypeError', function() {
+      var self = this;
+      expect(function() {
+        self.eb.getAllRelayEndpointSpecs();
+      }).toThrowError(TypeError);
+    });
+
+  });
+
+  describe('.sendToRelay', function() {
+    var msg = { 'volume': 12 };
+
+    beforeEach(function() {
+      this.handleRelayData = jasmine.createSpy('handleRelayData');
+      this.eb = new EventBus();
+      this.uid1 = this.eb.addRelayEndpoint('instrument', dataDef, this);
+      this.uid2 = this.eb.addRelayEndpoint('instrument', dataDef, this);
+    });
+
+    it('on success sends a message to all relay endpoints', function() {
+      this.eb.sendToRelay('instrument', msg);
+      expect(this.handleRelayData).toHaveBeenCalledTimes(2);
+      expect(this.handleRelayData).toHaveBeenCalledWith(msg);
+    });
+
+    it('fails silently with wrong uid', function() {
+      var self = this;
+      expect(function() {
+        self.eb.sendToRelay('foo', msg);
+      }).not.toThrow();
+    });
+
+    it('fails silently without a msg', function() {
+      var self = this;
+      expect(function() {
+        self.eb.sendToRelay('foo');
+      }).not.toThrow();
+    });
+
+    it('fails silently without any arguments', function() {
+      var self = this;
+      expect(function() {
+        self.eb.sendToRelay();
+      }).not.toThrow();
+    });
+
+  });
+
+  describe('.sendToRelayEndpoint', function() {
+    var msg = { 'volume': 12 };
+
+    beforeEach(function() {
+      this.handleRelayData = jasmine.createSpy('handleRelayData');
+      this.eb = new EventBus();
+      this.uid1 = this.eb.addRelayEndpoint('instrument', dataDef, this);
+      this.uid2 = this.eb.addRelayEndpoint('instrument', dataDef, this);
+    });
+
+    it('on success sends a message to a specific endpoint', function() {
+      this.eb.sendToRelayEndpoint(this.uid1, msg);
+      expect(this.handleRelayData).toHaveBeenCalledTimes(1);
+    });
+
+    it('fails silently with wrong uid', function() {
+      var self = this;
+      expect(function() {
+        self.eb.sendToRelayEndpoint('foo', msg);
+      }).not.toThrow();
+    });
+
+    it('fails silently without a message', function() {
+      var self = this;
+      expect(function() {
+        self.eb.sendToRelayEndpoint('foo');
+      }).not.toThrow();
+    });
+
+    it('fails silently without any argument', function() {
+      var self = this;
+      expect(function() {
+        self.eb.sendToRelayEndpoint();
+      }).not.toThrow();
+    });
+
+  });
+
+  describe('.getRelayNames', function() {
+
+    beforeEach(function() {
+      this.eb = new EventBus();
+      this.names = this.eb.getRelayNames();
+    });
+
+    it('returns an array with all relay names', function() {
+      expect(this.names.length).toEqual(2);
+      expect(this.names).toContain('instrument');
+      expect(this.names).toContain('fx');
+    });
+
+  });
+
+  describe('.subscribe', function() {
+
+    beforeEach(function() {
+      this.eb = new EventBus();
+    });
+
+    describe('on success', function() {
+
+      beforeEach(function() {
+        this.callback = jasmine.createSpy('callback');
+        this.eb.subscribe('onRelayAdd', this.callback, this);
+      });
+
+      it('adds a subscriber object to the array of this message', function() {
+        var subscriber = this.eb.messages.onRelayAdd[0];
+        expect(subscriber).toEqual({ 'context': this, 'fn': this.callback });
+      });
+
+    });
+
+    describe('on failure', function() {
+      var self;
+
+      beforeEach(function() {
+        self = this;
+        this.callback = jasmine.createSpy('callback');
+      });
+
+      it('if context is missing, throws TypeError', function() {
+        expect(function() {
+          self.subscribe('onRelayAdd', self.callback);
+        }).toThrowError(TypeError);
+      });
+
+      it('if fn is not a function, throws TypeError', function() {
+        expect(function() {
+          self.subscribe('onRelayAdd', 'foo', this);
+        }).toThrowError(TypeError);
+      });
+
+      it('if msg is not a string, throws TypeError', function() {
+        expect(function() {
+          self.subscribe({}, self.callback, this);
+        }).toThrowError(TypeError);
+      });
+
+    });
+  });
+
+  describe('.unsubscribe', function() {
+
+    beforeEach(function() {
+      this.eb = new EventBus();
+      this.callback = jasmine.createSpy('callback');
+      this.eb.subscribe('onRelayAdd', this.callback, this);
+    });
+
+    describe('on success', function() {
+
+      it('removes an object from a message array', function() {
+        this.eb.unsubscribe('onRelayAdd', this);
+        expect(this.eb.messages.onRelayAdd.length).toEqual(0);
+      });
+    });
+
+    describe('on failure', function() {
+
+      it('throws a TypeError if context is undefined', function() {
+        expect(function() {
+          this.eb.unsubscribe('onRelayAdd');
+        }).toThrowError(TypeError);
+      });
+
+      it('fails silently if context is not found in msg array', function() {
+        this.eb.unsubscribe('onRelayAdd', 'sdf');
+        expect(this.eb.messages.onRelayAdd.length).toEqual(1);
+      });
+
+      it('throws a TypeError if msg is not a property of eb.messages', function() {
+        expect(function() {
+          this.eb.unsubscribe('sdf', this);
+        }).toThrowError(TypeError);
+      });
+
+    });
+
+  });
+
+  describe('.sendMessage', function() {
+    var fakeThis = { 'a': 1, 'b': 2 };
+    var anotherThis = { 'c': 3, 'd': 4 };
+
+    beforeEach(function() {
+      this.callback = jasmine.createSpy('callback');
+      this.eb = new EventBus();
+      this.eb.subscribe('onRelayAdd', this.callback, this);
+      this.eb.subscribe('onRelayAdd', this.callback, fakeThis);
+      this.eb.subscribe('onRelayAdd', this.callback, anotherThis);
+    });
+
+    describe('on success', function() {
+
+      beforeEach(function() {
+        this.eb.sendMessage('onRelayAdd', 'a message');
+      });
+
+      it('calls the callback of all subscribers', function() {
+        expect(this.callback).toHaveBeenCalledTimes(3);
+      });
+
+      it('calls the callbacks with the given message', function() {
+        expect(this.callback).toHaveBeenCalledWith('a message');
+      });
+
+    });
+
+    describe('on failure', function() {
+
+      it('if msg arg is not valid throws a TypeError', function() {
+        expect(function() {
+          this.eb.sendMessage('foo', 'a message');
+        }).toThrowError(TypeError);
+      });
+
+      it('if no argument give, calls the callbacks with "undefined"', function() {
+        this.eb.sendMessage('onRelayAdd');
+        expect(this.callback).toHaveBeenCalledWith(undefined);
+      });
 
     });
 
