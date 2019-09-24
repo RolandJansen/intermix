1e10b435452554b21032bfac6aa23c3d18e54d7d
RolandJansen <jansen@stereokrauts.com>
2019-09-24 18:35:34 +0200

    webpack build system

--- src/plugins/Sequencer/scheduleWorker.ts
+++ src/plugins/Sequencer/scheduleWorker.ts
@@ -1,18 +1,12 @@
 /**
  * This is a webworker that provides a timer
  * that fires the scheduler for the sequencer.
- * This is because timing here is  more stable
+ * This is because timing here is more stable
  * than in the main thread.
- * The syntax is adapted to the commonjs module pattern.
- * @example <caption>It is just for library internal
- * usage. See Sequencer.js for details.</caption>
- * worker.postMessage({ 'interval': 200 });
- * worker.postMessage('start');
- * worker.postMessage('stop');
- * worker.terminate();  //webworker internal function, just for completeness
+ * The syntax is slidely different from vanilla workers
+ * because of typescript and webpack.
+ * Usage: See Sequencer.js for details
  */
-'use strict';
-
 var timer = null;
 var interval = 100;
 
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DtsBundleWebpack = require('dts-bundle-webpack');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        intermix: './src/index.ts',
+        // demo: './src/demo/demo.ts'
+    },
+    output: {
+        devtoolModuleFilenameTemplate: '[absolute-resource-path]',  // for vs debugger
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js',
+        library: '[name]',
+        libraryTarget: 'umd',
+        globalObject: 'this'
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /(node-modules|bower_components)/
+            }
+        ]
+    },
+    devtool: 'cheap-module-source-map',  // eval based sm doesn't work with vs debugger
+    plugins: [
+        new DtsBundleWebpack({
+            // see dts-bundle-webpack npm page for options
+            name: 'Intermix',
+            main: './src/index.ts',
+            out: '../dist/intermix.d.ts',
+            removeSource: true,
+            outputAsModuleFolder: true
+        }),  // bundle type files
+        new webpack.NamedModulesPlugin(),  // clean build logs
+        // new HtmlWebpackPlugin({
+        //     template: './src/demo/demo.html'
+        // }),  // use a html template for the demo
+        new webpack.HotModuleReplacementPlugin()  // use hot resync
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        hot: true
+    }
+};
