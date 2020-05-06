1ce0670294fc786acb80d454fffe5c8e552d6ffd
RolandJansen <jansen@stereokrauts.com>
2020-05-06 19:00:16 +0200

    demo samples loading

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,4 +1,4 @@
-// tslint:disable:no-var-requires
-export const skophat = require("./assets/Casio SK-1/skophat.wav") as ArrayBuffer;
-export const sksnamre = require("./assets/Casio SK-1/sksnare.wav") as ArrayBuffer;
 export const skkick = require("./assets/Casio SK-1/skkick.wav") as ArrayBuffer;
+export const sksnare = require("./assets/Casio SK-1/sksnare.wav") as ArrayBuffer;
+export const skclhat = require("./assets/Casio SK-1/skclhat.wav") as ArrayBuffer;
+export const skophat = require("./assets/Casio SK-1/skophat.wav") as ArrayBuffer;
