aa9bd5a494d6e50d87fa6990036e15bc09966581
Roland Jansen <mail@roland-jansen.de>
2020-07-09 17:35:30 +0200

    deepCopy algorithm in place

--- src/__tests__/helper.test.ts
+++ src/__tests__/helper.test.ts
@@ -1,4 +1,4 @@
-import { getRandomString, isKeyUnique } from "../helper";
+import { getRandomString, isKeyUnique, deepCopy } from "../helper";
 
 describe("getRandomString()", () => {
     test("returns a random string of variable length", () => {
@@ -26,5 +26,98 @@ describe("isKeyUnique()", () => {
     test("returns true if key is not in hashmap", () => {
         const keyIsUnique = isKeyUnique("ghi", hashmap);
         expect(keyIsUnique).toBeTruthy();
+    });
+});
+
+describe("deepCopy()", () => {
+    const testArrayFlat: number[] = [23, 42, 237];
+    const testArrayNested: number[][] = [[23], [42], [237]];
+    const testObjectFlat = {
+        one: 1,
+        two: "zwei",
+        three: true,
+    };
+    const testObjectNested = {
+        one: { two: "zwei" },
+        three: { drei: true },
+    };
+
+    const testMixedArray: any = ["astring", 23, testArrayNested, testObjectNested];
+
+    const testMixedObject: any = {
+        one: "eins",
+        two: 2,
+        superList: testArrayNested,
+        superDuperList: testMixedArray,
+        superDuperObject: { testObjectNested },
+    };
+
+    test("returns null if null is given", () => {
+        const testValue = null;
+        const result = deepCopy(testValue);
+        expect(result).toBe(testValue);
+    });
+
+    test("copies numbers", () => {
+        const testValue = 23;
+        const result = deepCopy(testValue);
+        expect(typeof result).toMatch("number");
+        expect(result).toEqual(testValue);
+    });
+
+    test("copies strings", () => {
+        const testValue = "hello";
+        const result = deepCopy(testValue);
+        expect(typeof result).toMatch("string");
+        expect(result).toEqual(testValue);
+    });
+
+    test("copies booleans", () => {
+        const testValue = true;
+        const result = deepCopy(testValue);
+        expect(typeof result).toMatch("boolean");
+        expect(result).toEqual(testValue);
+    });
+
+    test("copies date objects", () => {
+        const testValue = new Date();
+        const result = deepCopy(testValue);
+        expect(result).toEqual(testValue);
+        expect(result).not.toBe(testValue);
+    });
+
+    test("copies flat arrays", () => {
+        const result = deepCopy(testArrayFlat);
+        expect(result).toEqual(testArrayFlat);
+        expect(result).not.toBe(testArrayFlat);
+    });
+
+    test("copies flat objects", () => {
+        const result = deepCopy(testObjectFlat);
+        expect(result).toEqual(testObjectFlat);
+    });
+
+    test("copies nested arrays", () => {
+        const result = deepCopy(testArrayNested);
+        expect(result).toEqual(testArrayNested);
+        expect(result).not.toBe(testArrayNested);
+    });
+
+    test("copies nested objects", () => {
+        const result = deepCopy(testObjectNested);
+        expect(result).toEqual(testObjectNested);
+        expect(result).not.toBe(testObjectNested);
+    });
+
+    test("copies mixed arrays", () => {
+        const result = deepCopy(testMixedArray);
+        expect(result).toEqual(testMixedArray);
+        expect(result).not.toBe(testMixedArray);
+    });
+
+    test("copies mixed objects", () => {
+        const result = deepCopy(testMixedObject);
+        expect(result).toEqual(testMixedObject);
+        expect(result).not.toBe(testMixedObject);
     });
 });
--- src/helper.ts
+++ src/helper.ts
@@ -18,3 +18,45 @@ export const isKeyUnique = (key: string, hashMap: object): boolean => {
     }
     return true;
 };
+
+/**
+ * Copies deeply nested (and other) data by value.
+ * It's based on a gist by Erik Vullings,
+ * the author of "deep-copy-ts".
+ * see https://gist.github.com/erikvullings/ada7af09925082cbb89f40ed962d475e
+ * @param original The value to be cloned of any type
+ */
+export const deepCopy = <T>(original: T): T => {
+    // null should be handled before objects.
+    // Otherwise an empty object would be returned.
+    if (original === null) {
+        return original;
+    }
+
+    if (original instanceof Date) {
+        return new Date(original.getTime()) as any;
+    }
+
+    if (original instanceof Array && original.length !== 0) {
+        const clone: T = original.map(<N>(arrayElement: N) => {
+            return deepCopy(arrayElement);
+        }) as any;
+        return clone;
+    }
+
+    // objects have to be handled last since all other values
+    // are objects, too.
+    if (typeof original === "object" && original !== {}) {
+        // not very efficient to shallow copy and then to deep copy
+        // but didn't figure out how to initialize "clone" otherwise
+        const clone: T = Object.assign({}, original);
+        for (const key in original) {
+            clone[key] = deepCopy(original[key]);
+        }
+        return clone;
+    }
+
+    // if its not an object/array/date,
+    // it must be a primitive, empty array/object
+    return original;
+};
