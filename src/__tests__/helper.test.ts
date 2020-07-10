baa99f46603b7cf8cd34438d963694f73e604d30
Roland Jansen <mail@roland-jansen.de>
2020-07-10 05:29:41 +0200

    deepCopy now handles ArrayBuffer, TypedArrays, RegExp and undefined

--- src/__tests__/helper.test.ts
+++ src/__tests__/helper.test.ts
@@ -1,4 +1,4 @@
-import { getRandomString, isKeyUnique, deepCopy } from "../helper";
+import { getRandomString, isKeyUnique, deepCopy, TypedArray } from "../helper";
 
 describe("getRandomString()", () => {
     test("returns a random string of variable length", () => {
@@ -41,9 +41,7 @@ describe("deepCopy()", () => {
         one: { two: "zwei" },
         three: { drei: true },
     };
-
     const testMixedArray: any = ["astring", 23, testArrayNested, testObjectNested];
-
     const testMixedObject: any = {
         one: "eins",
         two: 2,
@@ -52,10 +50,24 @@ describe("deepCopy()", () => {
         superDuperObject: { testObjectNested },
     };
 
-    test("returns null if null is given", () => {
-        const testValue = null;
+    test("returns undefined if undefined is given", () => {
+        const testValue = undefined;
         const result = deepCopy(testValue);
         expect(result).toBe(testValue);
+    });
+
+    test("returns null if null is given", () => {
+        let testValue = null;
+        const result = deepCopy(testValue);
+        expect(result).toBe(testValue);
+        testValue = 5; // just to enshure that it's not the same
+        expect(result).toBe(null);
+    });
+
+    test("returns infinity if inf is given", () => {
+        const testValue = Infinity;
+        const result = deepCopy(testValue);
+        expect(result).toBe(Infinity);
     });
 
     test("copies numbers", () => {
@@ -81,6 +93,20 @@ describe("deepCopy()", () => {
 
     test("copies date objects", () => {
         const testValue = new Date();
+        const result = deepCopy(testValue);
+        expect(result).toEqual(testValue);
+        expect(result).not.toBe(testValue);
+    });
+
+    test("copies regular expressions", () => {
+        const testValue = new RegExp(/ab+c/, "i");
+        const result = deepCopy(testValue);
+        expect(result).toEqual(testValue);
+        expect(result).not.toBe(testValue);
+    });
+
+    test("copies array buffers", () => {
+        const testValue = new ArrayBuffer(8);
         const result = deepCopy(testValue);
         expect(result).toEqual(testValue);
         expect(result).not.toBe(testValue);
@@ -119,5 +145,36 @@ describe("deepCopy()", () => {
         const result = deepCopy(testMixedObject);
         expect(result).toEqual(testMixedObject);
         expect(result).not.toBe(testMixedObject);
+    });
+
+    // TypedArrays are array-like "views" for ArrayBuffers
+    // we create then with data from a flat array
+    describe("typed arrays", () => {
+        interface ITypedArrays {
+            [propName: string]: TypedArray;
+        }
+
+        const typedArrays: ITypedArrays = {
+            Int8Array: new Int8Array(testArrayFlat),
+            Uint8Array: new Uint8Array(testArrayFlat),
+            Uint8ClampedArray: new Uint8ClampedArray(testArrayFlat),
+            Int16Array: new Int16Array(testArrayFlat),
+            Uint16Array: new Uint16Array(testArrayFlat),
+            Int32Array: new Int32Array(testArrayFlat),
+            Uint32Array: new Uint32Array(testArrayFlat),
+            Float32Array: new Float32Array(testArrayFlat),
+            Float64Array: new Float64Array(testArrayFlat),
+            BigInt64Array: new BigInt64Array(8),
+            BigUint64Array: new BigUint64Array(8),
+        };
+
+        for (const key in typedArrays) {
+            test(`copies ${key}`, () => {
+                const testValue = typedArrays[key];
+                const result = deepCopy(testValue);
+                expect(result).toEqual(testValue);
+                expect(result).not.toBe(testValue);
+            });
+        }
     });
 });
--- src/helper.ts
+++ src/helper.ts
@@ -19,22 +19,54 @@ export const isKeyUnique = (key: string, hashMap: object): boolean => {
     return true;
 };
 
+// this should be in the types or interfaces or sth
+export type TypedArray =
+    | Int8Array
+    | Uint8Array
+    | Int16Array
+    | Uint16Array
+    | Int32Array
+    | Uint32Array
+    | Uint8ClampedArray
+    | Float32Array
+    | Float64Array
+    | BigInt64Array
+    | BigUint64Array;
+
 /**
  * Copies deeply nested (and other) data by value.
- * It's based on a gist by Erik Vullings,
- * the author of "deep-copy-ts".
+ * It's based on a gist by Erik Vullings, the author of "deep-copy-ts"
+ * with a few improvements and additions for complex types
  * see https://gist.github.com/erikvullings/ada7af09925082cbb89f40ed962d475e
  * @param original The value to be cloned of any type
  */
 export const deepCopy = <T>(original: T): T => {
-    // null should be handled before objects.
-    // Otherwise an empty object would be returned.
-    if (original === null) {
+    // what about:
+    // * maps
+    // * sets
+    // * img data
+    // are the results still of same type after casting to any?
+
+    // null and undefined (==) should be returned not copied.
+    if (original == null) {
         return original;
     }
 
     if (original instanceof Date) {
         return new Date(original.getTime()) as any;
+    }
+
+    if (original instanceof RegExp) {
+        return new RegExp(original.source, original.flags) as any;
+    }
+
+    if (original instanceof ArrayBuffer) {
+        return original.slice(0, original.byteLength) as any;
+    }
+
+    // TypedArrays are views for ArrayBuffers (binary data)
+    if (isTypedArray(original)) {
+        return copyTypedArray(original) as any;
     }
 
     if (original instanceof Array && original.length !== 0) {
@@ -59,4 +91,51 @@ export const deepCopy = <T>(original: T): T => {
     // if its not an object/array/date,
     // it must be a primitive, empty array/object
     return original;
+};
+
+const copyTypedArray = (original: TypedArray): TypedArray => {
+    if (original instanceof Int8Array) {
+        return new Int8Array(original);
+    }
+    if (original instanceof Uint8Array) {
+        return new Uint8Array(original);
+    }
+    if (original instanceof Uint8ClampedArray) {
+        return new Uint8ClampedArray(original);
+    }
+    if (original instanceof Int16Array) {
+        return new Int16Array(original);
+    }
+    if (original instanceof Uint16Array) {
+        return new Uint16Array(original);
+    }
+    if (original instanceof Int16Array) {
+        return new Int16Array(original);
+    }
+    if (original instanceof Uint16Array) {
+        return new Uint16Array(original);
+    }
+    if (original instanceof Int32Array) {
+        return new Int32Array(original);
+    }
+    if (original instanceof Uint32Array) {
+        return new Uint32Array(original);
+    }
+    if (original instanceof Float32Array) {
+        return new Float32Array(original);
+    }
+    if (original instanceof Float64Array) {
+        return new Float64Array(original);
+    }
+    if (original instanceof BigInt64Array) {
+        return new BigInt64Array(original);
+    }
+    if (original instanceof BigUint64Array) {
+        return new BigUint64Array(original);
+    }
+    return new Uint8Array(); // just to return something from every path
+};
+
+const isTypedArray = (item: any): item is TypedArray => {
+    return (item as TypedArray).buffer !== undefined;
 };
