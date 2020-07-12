814c6e327f3442763c2448423c9714028323d117
Roland Jansen <mail@roland-jansen.de>
2020-07-12 16:22:25 +0200

    SeqPart reducer logic moved to seperate file

--- src/registry/commonActionDefs.ts
+++ src/registry/commonActionDefs.ts
@@ -17,6 +17,16 @@ const commonActionDefs: IOscActionDef[] = [
         value: ["volume", 0.0, 0.0],
         description: "name, loudness-value, starttime",
     },
+    {
+        address: "intermix/plugin/{UID}/loadPreset",
+        typeTag: "i",
+        description: "loads a preset and changes the preset number accordingly",
+    },
+    {
+        address: "intermix/plugin/{UID}/savePreset",
+        typeTag: "i",
+        description: "saves all properties defined in 'actionDefs' and changes the preset number accordingly",
+    },
 ];
 
 export default commonActionDefs;
