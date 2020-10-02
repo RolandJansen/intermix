ff6ec99f354006916ec32a28414ea895edbb72a3
Roland Jansen <mail@roland-jansen.de>
2020-10-02 23:17:57 +0200

    Delete demo.ts

--- demo/demo.ts
+++
@@ -1,4 +0,0 @@
-export const skkick = require("./assets/Casio SK-1/skkick.wav") as ArrayBuffer;
-export const sksnare = require("./assets/Casio SK-1/sksnare.wav") as ArrayBuffer;
-export const skclhat = require("./assets/Casio SK-1/skclhat.wav") as ArrayBuffer;
-export const skophat = require("./assets/Casio SK-1/skophat.wav") as ArrayBuffer;
